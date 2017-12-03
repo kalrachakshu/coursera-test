565a171e7199dd1a555a9c68b2f049b4e1dd0213
kalrachakshu <33355182+kalrachakshu@users.noreply.github.com>
2017-12-03 17:50:02 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,7 +1,7 @@
 
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John","Ajay","Josh","James", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
 var firstLetter = names[i].charAt(0).toLowerCase();
 
